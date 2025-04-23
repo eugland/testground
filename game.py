2f7ed03c2d9ace4e34018ee6032b766853bd8c73
eugen <eugene.r.w.12@gmail.com>
2025-04-23 04:38:40 -0700

    Add loop for multiple decisions

--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 def main():
     name = input('Enter your name: ')
     health = 100
-    choice = input('Do you go left or right? ')
-    if choice == 'left':
-        print('You encounter a friendly dragon!')
-    else:
-        print('You fall into a trap!')
-        health -= 50
-    print(f'{name} has {health} health points.')
+    while health > 0:
+        choice = input('Do you go left or right? ')
+        if choice == 'left':
+            print('You encounter a friendly dragon!')
+        else:
+            print('You fall into a trap!')
+            health -= 50
+        print(f'{name} has {health} health points.')
+    print('Game over!')
 
 main()
